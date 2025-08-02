0bf84f257149a107c134c5c0833ba4f51238af26
eclipseeer <eclipseeer@gmail.com>
2025-08-02 13:57:30 +0300

    Rework sign fn

--- packages/core/src/common/crypto/sign.ts
+++ packages/core/src/common/crypto/sign.ts
@@ -1,16 +1,15 @@
-import type { PrivateKey } from '@types';
+import * as v from 'valibot';
+import type { PrivateKey, Hex } from '@types';
 import { ed25519 } from '@noble/curves/ed25519';
 import { secp256k1 } from '@noble/curves/secp256k1';
 import { fromCurveString, toCurveString } from './curveString';
-import { BinaryCryptoKeyLengths } from '../../configs/constants';
+import {
+  binarySecp256k1PrivateKeySchema,
+  binaryEd25519PrivateKeySchema,
+} from '@schemas/valibot';
 
-type SignArgs = {
-  message: Uint8Array;
-  privateKey: PrivateKey;
-};
-
-const signByEd25519Key = (message: Uint8Array, u8PrivateKey: Uint8Array) => {
-  const u8SecretKey = u8PrivateKey.slice(0, BinaryCryptoKeyLengths.Ed25519.SecretKey);
+const signByEd25519Key = (message: Hex, u8PrivateKey: Uint8Array) => {
+  const { u8SecretKey } = v.parse(binaryEd25519PrivateKeySchema, u8PrivateKey);
   const u8Signature = ed25519.sign(message, u8SecretKey);
 
   return {
@@ -19,11 +18,10 @@ const signByEd25519Key = (message: Uint8Array, u8PrivateKey: Uint8Array) => {
   };
 };
 
-const signBySecp256k1Key = (message: Uint8Array, u8PrivateKey: Uint8Array) => {
-  // Create getSecretKey
-  const u8SecretKey = u8PrivateKey.slice(
-    0,
-    BinaryCryptoKeyLengths.Secp256k1.SecretKey,
+const signBySecp256k1Key = (message: Hex, u8PrivateKey: Uint8Array) => {
+  const { u8SecretKey } = v.parse(
+    binarySecp256k1PrivateKeySchema,
+    u8PrivateKey,
   );
   const signatureObj = secp256k1.sign(message, u8SecretKey);
 
@@ -38,10 +36,14 @@ const signBySecp256k1Key = (message: Uint8Array, u8PrivateKey: Uint8Array) => {
   };
 };
 
-// TODO maybe use Hex instead of Uint8Array for message
+type SignArgs = {
+  message: Hex;
+  privateKey: PrivateKey;
+};
+
 export const sign = ({ message, privateKey }: SignArgs) => {
-  // PrivateKeySchema.parse(privateKey);
   const { curve, u8Data: u8PrivateKey } = fromCurveString(privateKey);
-  if (curve === 'secp256k1') return signBySecp256k1Key(message, u8PrivateKey);
-  return signByEd25519Key(message, u8PrivateKey);
+  return curve === 'ed25519'
+    ? signByEd25519Key(message, u8PrivateKey)
+    : signBySecp256k1Key(message, u8PrivateKey);
 };
--- packages/core/src/types/common.ts
+++ packages/core/src/types/common.ts
@@ -8,3 +8,9 @@ type ByFinality = { finality?: Finality; blockId?: never };
 type ByBlockId = { finality?: never; blockId?: BlockId };
 
 export type BlockTarget = ByFinality | ByBlockId;
+
+/**
+ * Represents data encoded in hexadecimal format.
+ * Can be either a raw byte array (Uint8Array) or a hexadecimal string (e.g., "deadbeef" or "0xdeadbeef").
+ */
+export type Hex = Uint8Array | string;
