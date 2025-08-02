29b515445896337c544db153fb71abe19f7b3db2
eclipseeer <eclipseeer@gmail.com>
2025-08-02 13:32:12 +0300

    Rework Private Key validations

--- packages/core/src/common/crypto/getPublicKey.ts
+++ packages/core/src/common/crypto/getPublicKey.ts
@@ -1,19 +1,27 @@
+import * as v from 'valibot';
 import type { PublicKey, PrivateKey } from '@types';
-import { BinaryCryptoKeyLengths } from '../../configs/constants';
 import { toCurveString, fromCurveString } from './curveString';
+import {
+  binarySecp256k1PrivateKeySchema,
+  binaryEd25519PrivateKeySchema,
+} from '@schemas/valibot';
+
+const getEd25519PublicKey = (u8PrivateKey: Uint8Array) => {
+  const { u8PublicKey } = v.parse(binaryEd25519PrivateKeySchema, u8PrivateKey);
+  return toCurveString('ed25519', u8PublicKey);
+};
+
+const getSecp256k1PublicKey = (u8PrivateKey: Uint8Array) => {
+  const { u8PublicKey } = v.parse(
+    binarySecp256k1PrivateKeySchema,
+    u8PrivateKey,
+  );
+  return toCurveString('secp256k1', u8PublicKey);
+};
 
 export const getPublicKey = (privateKey: PrivateKey): PublicKey => {
   const { curve, u8Data: u8PrivateKey } = fromCurveString(privateKey);
-
-  if (curve === 'secp256k1') {
-    return toCurveString(
-      curve,
-      u8PrivateKey.slice(BinaryCryptoKeyLengths.Secp256k1.SecretKey), // public key = last 64 bytes
-    );
-  }
-
-  return toCurveString(
-    curve,
-    u8PrivateKey.slice(BinaryCryptoKeyLengths.Ed25519.SecretKey), // public key = last 32 bytes
-  );
+  return curve === 'ed25519'
+    ? getEd25519PublicKey(u8PrivateKey)
+    : getSecp256k1PublicKey(u8PrivateKey);
 };
--- packages/core/src/schemas/valibot/crypto.ts
+++ packages/core/src/schemas/valibot/crypto.ts
@@ -1,7 +1,7 @@
 import * as v from 'valibot';
-import { pipe } from 'valibot';
+import { BinaryCryptoKeyLengths } from '../../configs/constants';
 
-export const base58StringSchema = pipe(
+export const base58StringSchema = v.pipe(
   v.string(),
   v.regex(
     /^[1-9A-HJ-NP-Za-km-z]+$/,
@@ -10,6 +10,28 @@ export const base58StringSchema = pipe(
   ),
 );
 
+const { Ed25519, Secp256k1 } = BinaryCryptoKeyLengths;
 
-// Ed25 private key length
-// Secp251k1 private key length
+export const binarySecp256k1PrivateKeySchema = v.pipe(
+  v.instance(Uint8Array),
+  v.length(
+    Secp256k1.PrivateKey,
+    `Length of binary Secp256k1 private key should be ${Secp256k1.PrivateKey}`,
+  ),
+  v.transform((u8PrivateKey) => ({
+    u8SecretKey: u8PrivateKey.slice(0, Secp256k1.SecretKey),
+    u8PublicKey: u8PrivateKey.slice(Secp256k1.SecretKey),
+  })),
+);
+
+export const binaryEd25519PrivateKeySchema = v.pipe(
+  v.instance(Uint8Array),
+  v.length(
+    Ed25519.PrivateKey,
+    `Length of binary Ed25519 private key should be ${Ed25519.PrivateKey}`,
+  ),
+  v.transform((u8PrivateKey) => ({
+    u8SecretKey: u8PrivateKey.slice(0, Ed25519.SecretKey),
+    u8PublicKey: u8PrivateKey.slice(Ed25519.SecretKey),
+  })),
+);
