1df7e2aa01d7c2d7b652ec9485aba160a7e43042
eclipseeer <eclipseeer@gmail.com>
2025-09-05 12:43:08 +0300

    Add basic callFn overload example

--- examples/nodejs/ts-esm/src/index.ts
+++ examples/nodejs/ts-esm/src/index.ts
@@ -7,71 +7,71 @@
 // import './transactions/deployContract';
 
 // import './calls/getAccount';
-import './calls/callContractReadFunction';
+// import './calls/callContractReadFunction';
 
 // import './transactions/signer/transfer';
 //import './transactions/signer/functionCall';
 
-// type BaseTransformFn = (v: number[]) => unknown
-//
-// type Args<A, F extends BaseTransformFn> = {
-//   jsonArgs: A;
-//   transformFn?: F;
-// };
-//
-// type Result<A, F extends BaseTransformFn> = {
-//   result: ReturnType<F>;
-// };
-//
-// const defaultTransformFn = (v: number[]): unknown => {
-//   return 'something';
-// };
-//
-// const customTransformFn = (v: number[]): { a: number } => {
-//   return { a: 1 };
-// };
-//
-// const callFn = <A, F>(args: Args<A>): Result<A, F> => {
-//   const transformFn = args?.transformFn ?? defaultTransformFn;
-//   return transformFn([1]);
-// };
-//
-// const res1 = callFn({ jsonArgs: {} })
-// console.log(res1);
-
 type DefaultTransformFn = (v: number[]) => unknown;
-
-type Result<F extends DefaultTransformFn | undefined> = [F] extends [
-  DefaultTransformFn,
-]
-  ? {
-      result: ReturnType<F>;
-    }
-  : { result: unknown };
-
-type Args<A, F extends DefaultTransformFn | undefined> = {
-  jsonArgs: A;
-} & ([F] extends [DefaultTransformFn]
-  ? { transformFn: F }
-  : { transformFn?: never });
-
 const defaultTransformFn: DefaultTransformFn = (_v) => 'something';
 
-const callFn = <A, F extends DefaultTransformFn | undefined = undefined>(
-  args: Args<A, F>,
-): Result<F> => {
-  const transform = args.transformFn ?? defaultTransformFn; // невеликий cast лишається
-  return { result: transform([1]) } as Result<F>;
+type Args<A, F extends DefaultTransformFn | undefined> = [F] extends [
+  DefaultTransformFn,
+]
+  ? { jsonArgs: A; transformFn: F }
+  : { jsonArgs: A };
+
+type Result<F extends DefaultTransformFn | unknown> = [F] extends [
+  DefaultTransformFn,
+]
+  ? { result: ReturnType<F> }
+  : { result: unknown };
+
+// Overload order matters
+type CallFn = {
+  <A, F extends DefaultTransformFn>(args: Args<A, F>): Result<F>;
+  <A>(args: Args<A, undefined>): Result<unknown>;
+};
+
+const callFn: CallFn = <A, F extends DefaultTransformFn>(args: {
+  jsonArgs: A;
+  transformFn?: F;
+}) => {
+  if (args.transformFn) {
+    return { result: args.transformFn([1]), b: 1 };
+  }
+  return { result: defaultTransformFn([1]) };
 };
 
 const customFn = (v: number[]) => ({ a: 1 });
+type InputArgs = { a: number };
 
-// Перевірки
-const r1 = callFn({ jsonArgs: {} }); // { result: unknown }
+// OK
+//  { result: unknown }
+const r1 = callFn({ jsonArgs: {} });
+// { result: { a: number } }
 const r2 = callFn({ jsonArgs: {}, transformFn: customFn });
-const r3 = callFn<{ b: number }>({ jsonArgs: { b: 1 } });
-
-const r4 = callFn<{ b: number }, (v: number[]) => { a: number }>({
-  jsonArgs: { b: 1 },
+// { result: unknown }
+const r3 = callFn<InputArgs>({ jsonArgs: { a: 1 } });
+//{ result: { a: number } }
+const r4 = callFn<InputArgs, typeof customFn>({
+  jsonArgs: { a: 1 },
   transformFn: customFn,
 });
+
+const args5 = {
+  jsonArgs: { a: 1 },
+  transformFn: customFn,
+};
+//{ result: { a: number } }
+const r5 = callFn(args5);
+
+// ERROR
+// const r100 = callFn<InputArgs>({
+//   jsonArgs: { a: 1 },
+//   transformFn: customFn,
+// });
+//
+// const r101 = callFn<{ b: number }, (v: number[]) => { a1: number }>({
+//   jsonArgs: { b: 1 },
+// });
