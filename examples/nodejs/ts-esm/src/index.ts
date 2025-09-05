fed23d2bb03a57d8368d3588ddd1dbcf83a39b96
eclipseeer <eclipseeer@gmail.com>
2025-09-05 14:46:42 +0300

    Add callFn overload with the contract interface example

--- examples/nodejs/ts-esm/src/index.ts
+++ examples/nodejs/ts-esm/src/index.ts
@@ -18,10 +18,10 @@ const defaultTransformFn: DefaultTransformFn = (_v) => 'something';
 type Args<A, F extends DefaultTransformFn | undefined> = [F] extends [
   DefaultTransformFn,
 ]
-  ? { jsonArgs: A; transformFn: F }
-  : { jsonArgs: A };
+  ? { fnArgs: A; transformFn: F }
+  : { fnArgs: A };
 
-type Result<F extends DefaultTransformFn | unknown> = [F] extends [
+type Result<F extends DefaultTransformFn | undefined> = [F] extends [
   DefaultTransformFn,
 ]
   ? { result: ReturnType<F> }
@@ -30,48 +30,96 @@ type Result<F extends DefaultTransformFn | unknown> = [F] extends [
 // Overload order matters
 type CallFn = {
   <A, F extends DefaultTransformFn>(args: Args<A, F>): Result<F>;
-  <A>(args: Args<A, undefined>): Result<unknown>;
+  <A>(args: Args<A, undefined>): Result<undefined>;
 };
 
 const callFn: CallFn = <A, F extends DefaultTransformFn>(args: {
-  jsonArgs: A;
+  fnArgs: A;
   transformFn?: F;
 }) => {
   if (args.transformFn) {
-    return { result: args.transformFn([1]), b: 1 };
+    return { result: args.transformFn([1]) };
   }
   return { result: defaultTransformFn([1]) };
 };
 
-const customFn = (v: number[]) => ({ a: 1 });
+const customFn = (_v: number[]) => ({ a: 1 });
 type InputArgs = { a: number };
 
 // OK
 //  { result: unknown }
-const r1 = callFn({ jsonArgs: {} });
+const r1 = callFn({ fnArgs: {} });
 // { result: { a: number } }
-const r2 = callFn({ jsonArgs: {}, transformFn: customFn });
+const r2 = callFn({ fnArgs: {}, transformFn: customFn });
 // { result: unknown }
-const r3 = callFn<InputArgs>({ jsonArgs: { a: 1 } });
+const r3 = callFn<InputArgs>({ fnArgs: { a: 1 } });
 //{ result: { a: number } }
 const r4 = callFn<InputArgs, typeof customFn>({
-  jsonArgs: { a: 1 },
+  fnArgs: { a: 1 },
   transformFn: customFn,
 });
 
 const args5 = {
-  jsonArgs: { a: 1 },
+  fnArgs: { a: 1 },
   transformFn: customFn,
 };
+// { result: unknown } // ????? It works well for old objects
+const r5 = callFn<InputArgs>(args5);
 //{ result: { a: number } }
-const r5 = callFn(args5);
+const r6 = callFn(args5);
+
 
 // ERROR
 // const r100 = callFn<InputArgs>({
-//   jsonArgs: { a: 1 },
+//   fnArgs: { a: 1 },
 //   transformFn: customFn,
 // });
 //
 // const r101 = callFn<{ b: number }, (v: number[]) => { a1: number }>({
-//   jsonArgs: { b: 1 },
+//   fnArgs: { b: 1 },
 // });
+
+
+/*
+const request = ftContractInterface.readFns.getFtBalance({
+  contractAccountId: 'ft.near',
+  fnArgs: { accountId: 'alice.near' },
+  options: {
+    finality: 'OPTIMISTIC', // optional, default: near-final
+  },
+});
+
+await client.callContractReadFunction(request);
+ */
+
+import * as z from 'zod/mini';
+
+const DataSchema = z.object({
+  nameFor: z.string(),
+});
+
+type DataType = z.output<typeof DataSchema>;
+
+const resultTransformer = (v: number[]): DataType => {
+  const obj = JSON.parse(new TextDecoder().decode(new Uint8Array(v)));
+  return DataSchema.parse(obj);
+};
+
+type GetFtBalanceResult = {
+  fnArgs: { account_id: string };
+  transformFn: (v: number[]) => DataType;
+};
+
+const getFtBalance = (args: { accountId: string }): GetFtBalanceResult => {
+  return {
+    fnArgs: {
+      account_id: args.accountId,
+    },
+    transformFn: resultTransformer,
+  };
+};
+
+// { result: { nameFor: string } }
+const r0 = callFn(getFtBalance({ accountId: '1d' }));
+
+const a = r0.result.nameFor
