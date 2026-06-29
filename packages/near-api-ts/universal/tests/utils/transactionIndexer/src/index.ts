078857514705d2e89b3d87e7fd7f69f49195ced4
eclipseeer <eclipseeer@gmail.com>
2026-06-29 18:33:25 +0300

    Update `loadTransactionsWithErrors` date range and increase `safeSleep` delay to 1 second for improved processing stability

--- packages/near-api-ts/universal/tests/utils/transactionIndexer/src/index.ts
+++ packages/near-api-ts/universal/tests/utils/transactionIndexer/src/index.ts
@@ -1,4 +1,4 @@
 import { loadTransactionsWithErrors } from './loadFailedTransactions/loadTransactionsWithErrors';
 import { getDateRange } from './utils/getDateRange';
 
-await loadTransactionsWithErrors(getDateRange('2020-11-11', '2021-03-01'));
+await loadTransactionsWithErrors(getDateRange('2020-11-16', '2021-03-01'));
--- packages/near-api-ts/universal/tests/utils/transactionIndexer/src/loadFailedTransactions/loadTransactionsWithErrors.ts
+++ packages/near-api-ts/universal/tests/utils/transactionIndexer/src/loadFailedTransactions/loadTransactionsWithErrors.ts
@@ -46,7 +46,7 @@ export const loadTransactionsWithErrors = async (days: string[]) => {
 
         logger.info({ processed, total: allRows.length, failed }, 'progress');
 
-        await safeSleep(500);
+        await safeSleep(1000);
       }
 
       logger.info({ total: allRows.length, failed }, 'day finished');
