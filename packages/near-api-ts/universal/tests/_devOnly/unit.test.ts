b74c18e0a73d84ee6a7be3901bf0057dc28c48b7
eclipseeer <eclipseeer@gmail.com>
2026-08-27 18:18:10 +0300

    test(transaction): convert a signed delegation borsh64 in the scratch test

--- packages/near-api-ts/universal/tests/_devOnly/unit.test.ts
+++ packages/near-api-ts/universal/tests/_devOnly/unit.test.ts
@@ -1,7 +1,12 @@
 import { it } from 'vitest';
-import { processingStageToFinalExecutionStatus } from '../../src/createClient/methods/transaction/_common/processingStageConverters';
+import { executeDelegation } from '../../index';
 import { log } from '../utils/common';
 
 it('unit test', () => {
-  log(processingStageToFinalExecutionStatus('ConvertedOptimistic'));
+  log(
+    executeDelegation({
+      signedDelegationBorsh64:
+        'EQAAAGVjbGlwc2Vlci50ZXN0bmV0HwAAAHJlYWN0LW5lYXItdHMubGFudHN0b29sLnRlc3RuZXQBAAAAAgoAAABhZGRfcmVjb3JkEAAAAHsicmVjb3JkIjoiMTIzIn0AoHJOGAkAAAAAAAAAAAAAAAAAAAAAAAAoiSoxZJAAADmi2A8AAAAAAFCo04xswi9H9mu+1Qe4czM3Fe/HnQ2hUZsmfuSntAsnANNsqRfiUdWyOaX1xd2zdJFCOTZCc3zI5cqoTXOIoPFIR1KDJfZVmaY9mcdWYhbBZNNsRKUv8bZ/K8Q5bCAu8AM=',
+    }),
+  );
 });
